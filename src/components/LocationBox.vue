793a3c2df3792cfd860d653ee0283e39d4a79c10
bryomajor <bryomajor@gmail.com>
2020-12-21 20:42:14 +0300

    split app into components

---
+++ src/components/LocationBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="location-box">
+    <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+    <div class="date">{{ now.format("dddd, Do MMMM YYYY") }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    weather: {
+      type: Object,
+      required: true
+    },
+    now: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25)
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+</style>
---
+++ src/components/WeatherWrap.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="weather-wrap" v-if="weather.main != undefined">
+    <location-box :now="now" :weather="weather" />
+    <weather-box :weather="weather" />
+  </div>
+</template>
+
+<script>
+import LocationBox from './LocationBox';
+import WeatherBox from './WeatherBox';
+
+export default {
+  components: {
+    LocationBox,
+    WeatherBox
+  },
+  props: {
+    weather: {
+      type: Object,
+      required: true
+    },
+    now: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
